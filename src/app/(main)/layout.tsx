deceefb81c6f111691f8afc819e10b2fa3879e97
datpq <datphamquoc92@gmail.com>
2025-06-22 11:03:22 +0700

    - Use another layout for login page

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,13 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
+export default function LoginLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <main>
+            main
+            <Header></Header>
+            {children}
+            <Footer />
+        </main>
+    );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,9 +41,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={clsx(nunito.className, nunitoSans.className)}>
       <body>
-        <Header></Header>
         {children}
-        <Footer />
         <div className="fixed bg-gradient-to-tr from-emerald-50 to-cyan-50 z-[-1]
           inset-0 opacity-50" />
         <PrismicPreview repositoryName={repositoryName} />

c22566355a1c4862553178f45465dd1dae2a4773
datpq <datphamquoc92@gmail.com>
2025-06-25 06:09:55 +0700

    - Sample for: how to get cookie from header of Server Component - Redirect to home page after login successfully

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,12 +1,19 @@
 import { CONSTANT } from "@/lib/constant";
 import { NextRequest, NextResponse } from "next/server";
+import { cookies } from 'next/headers'
 
 export async function GET(request: NextRequest) {
+  /* how to get cookie from header of Server Component */
+  const cookieStore = await cookies();
+  console.log("Cookies before logout:", cookieStore.getAll());
+
   const url = request.nextUrl.clone();
   url.pathname = "/login"; // Set redirect target
 
   const response = NextResponse.redirect(url);
   console.log("Logging out...");
+
+
 
   response.cookies.set(CONSTANT.AUTHENTICATION_COOKIE_NAME, "", {
     path: "/",

3f8aeb23e00ad131789f10c27c4beb02c21d0e9d
datpq <datphamquoc92@gmail.com>
2025-06-27 05:16:12 +0700

    - A few item in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // basePath: '/prismic-app' /* http://localhost:3000/prismic-app/about */
+  env: {
+    appName: 'Prismic App',
+  },
 };
 
 export default nextConfig;

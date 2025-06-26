19f79619aaa2301f4cc3866cfcdcbe6f74166bde
datpq <datphamquoc92@gmail.com>
2025-06-26 08:17:10 +0700

    - Add view port metadata

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,14 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import { Viewport } from "next";
+
+export const viewport: Viewport = {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+    userScalable: false,
+    themeColor: 'red',
+  }
 
 export default function LoginLayout({ children }: { children: React.ReactNode }) {
     return (

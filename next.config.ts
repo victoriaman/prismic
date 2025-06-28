1162112058c67f03adcc501ac36d8f8ca9f4ded8
datpq <datphamquoc92@gmail.com>
2025-06-28 08:21:23 +0700

    - Fix build failed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   // basePath: '/prismic-app' /* http://localhost:3000/prismic-app/about */
   env: {
     appName: 'Prismic App',
